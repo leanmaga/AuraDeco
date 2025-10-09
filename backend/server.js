6ba6bd4bfe37a7b55821acd3de66540f800330b7
Leandro Magallanes <lamagallanes@mobilecomputing.com.ar>
2025-10-09 11:37:38 -0300

    fix url

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   process.env.CLIENT_URL,
   "https://auradecodeco.vercel.app",
-  "https://aura-deco.vercel.app/",
+  "https://aura-deco.vercel.app",
 ].filter(Boolean);
 
 app.use(
